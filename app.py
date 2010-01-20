3e69a5b52daa6e7cee3fac595e07b86415727e5b
Conny Dahlgren <connyd@des-037.(none)>
2010-01-20 11:32:58 +0100

    config fix

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from twitter import twitter
 
 class KvitterApp:
     def __init__(self):
-        self.username_str = "sinex"
-        self.password_str = "ApVisp9X"
+        self.username_str = "username"
+        self.password_str = "password"
         self.api = twitter.Api(username=self.username_str, password=self.password_str, input_encoding=None)
         self.main_window = MainWindow()
 
